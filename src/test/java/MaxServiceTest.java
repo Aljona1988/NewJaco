37c5e379cf8e12f27b8d92affc218cb96bb91a8b
Elena Fedkina <zhrulick@maiil.ru>
2022-09-22 10:49:20 +0300

    test 2

--- src/test/java/MaxServiceTest.java
+++ src/test/java/MaxServiceTest.java
@@ -4,13 +4,28 @@ import org.junit.jupiter.api.Test;
 public class MaxServiceTest {
 
     @Test
-    public void  testShouldFindMax() {
+    public void testShouldFindMax() {
         MaxService service = new MaxService();
 
         int a = 5;
         int b = 3;
 
         int expected = a;
+        int actual = service.max(a, b);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+
+    public void testShouldFindMaxIfb() {
+
+        MaxService service = new MaxService();
+
+        int a = 3;
+        int b = 5;
+
+        int expected = b;
         int actual = service.max(a, b);
 
         Assertions.assertEquals(expected, actual);
